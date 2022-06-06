ca1a5d3157c56e4da182c5e741002ee0d4c532cb
unknown <segfaulty@proton.me>
2022-06-06 14:00:05 +0100

    fixed a paretheses bug added history nav

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,12 @@
+//TODO: show a list of features
+
+let history = {list:[],current : 0};
 const wrapper = document.querySelector(".calculator-wrapper");
 const screen = document.querySelector(".screen");
 const errorOutput = document.querySelector(".error");
 const opRegxG = /[-+*/]/;
 const opRegxLast = /[-+*/]$/;
 const opRegxFirst = /^[-+*/]/;
-const opRegxFirstOnce = /^[-+*/]/;
 
 
 function logError(msg, clr){
@@ -31,7 +33,13 @@ function calc(num1, num2, operator){
     if(operator == "+"){opResult = num1 + num2;}
     else if(operator == "-"){opResult = num1 - num2;}
     else if(operator == "*"){opResult = num1 * num2;}
-    else if(operator == "/"){opResult = num1 / num2;}
+    else if(operator == "/"){
+        if(num2 == 0){
+            if(num2 == num1){logError(`result is undefined!`, "red");return null;}
+            logError(`cannot devide by zero!`, "red");return null;
+        }
+        opResult = num1 / num2;
+    }
     return opResult;
 }
 
@@ -83,10 +91,14 @@ function parse(){
     
     let expression = screen.value;
     
+    //history
+    history.list.push(expression);
+    history.current = history.list.length;
+
     //check for ousider symbole
     let outsiderPosition = expression.search(/[^0-9-*+/().]/);
     if(outsiderPosition != -1){
-        logError(`outsider symbol (${expression[outsiderPosition]}), reformat your expression`, "red");
+        logError(`outsider symbol (${expression[outsiderPosition]}), reformat your expression`, "red");return;
     }
     
     //remove spaces
@@ -146,24 +158,32 @@ function parse(){
         //calc loop
         let calcResult = 0;
         let position = chunk.search(opRegxG);
-        if(position == -1){logError("no operator in the expression", "red");return;}
+        if(position == -1){
+            if(chunkLength==0){logError("no operator or oprand in the expression", "red");return;}
+        }
+
         while(position != -1){
             calcResult = calc(calcResult, parseFloat(chunk.slice(0, position)), operator);
+            if(calcResult == null){return;}
             operator = chunk[position];//set the new operator
             //clean
             chunk = chunk.slice(position+1);
             position = chunk.search(opRegxG);
-            console.log(chunk);
         }
 
-        chunk = String(calc(calcResult, parseFloat(chunk), operator));//calc the last operand && update result
+        //calc the last operand && update result
+        calcResult = calc(calcResult, parseFloat(chunk), operator);
+        if(calcResult == null){return;}
+        chunk = String(calcResult);
         expression = strReplace(expression, leftPar, chunkLength+2, chunk);//+2 to replace the parentheses if they exist, else it has no effect
 
         //so unprofessional, I have to learn RegEx
         if(expression.search(/[()]/) == -1 && (expression.search(opRegxG) == -1 || (expression.search(opRegxFirst) != -1 && expression.slice(1).search(opRegxG) == -1)))
         {break;}
     }
-
+    
+    let fpPos = expression.search(/[.]/);
+    if(fpPos != -1 && expression.length-fpPos-1 > 4){expression = expression.slice(0, fpPos+5);logError("the result was rounded because the floating point exceeded 4 digits");}
     screen.value = expression;
 }
 
@@ -201,13 +221,15 @@ function handleClick(e){
     
     logError("");//clear if there's any old error msg
 
-    if(e.target.getAttribute("data-calc") != null){parse();}
-    else if(e.target.getAttribute("data-operator") != null){validateOperator(e.target.getAttribute("data-operator"));}
-    else if(e.target.getAttribute("data-num") != null){screen.value += e.target.getAttribute("data-num");}
-    else if(e.target.getAttribute("data-fp") != null){validateFloatingPoint();}
-    else if(e.target.getAttribute("data-par") != null){validateParenthese(e.target);}
-    else if(e.target.getAttribute("data-del") != null){screen.value = screen.value.slice(0, screen.value.length-1);}
-    else if(e.target.getAttribute("data-clr") != null){screen.value = "";}
+         if(e.target.getAttribute("data-calc"     ) != null){parse();}
+    else if(e.target.getAttribute("data-operator" ) != null){validateOperator(e.target.getAttribute("data-operator"));}
+    else if(e.target.getAttribute("data-num"      ) != null){screen.value += e.target.getAttribute("data-num");}
+    else if(e.target.getAttribute("data-fp"       ) != null){validateFloatingPoint();}
+    else if(e.target.getAttribute("data-par"      ) != null){validateParenthese(e.target);}
+    else if(e.target.getAttribute("data-del"      ) != null){screen.value = screen.value.slice(0, screen.value.length-1);}
+    else if(e.target.getAttribute("data-clr"      ) != null){screen.value = "";}
+    else if(e.target.getAttribute("data-prev"     ) != null){(history.current>0) ? screen.value = history.list[--history.current] : function(){}}
+    else if(e.target.getAttribute("data-next"     ) != null){(history.current<history.list.length-1) ? screen.value = history.list[++history.current] : function(){}}
 }
 
 wrapper.addEventListener("click", handleClick);
